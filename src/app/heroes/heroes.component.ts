43fe4cb9a829b7b35448906df9df1c04ac78dc1f
Felix Jentsch <31540389+freilix@users.noreply.github.com>
2020-08-21 15:57:55 +0200

    added httpClient to get data from "server"

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,9 +21,19 @@ export class HeroesComponent implements OnInit {
     this.messageService.add("Hero '" + hero.name + "' selected");
   }
   */
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({name} as Hero).subscribe(hero => {this.heroes.push(hero)});
   }
 
   constructor(private heroService: HeroService) {}
